d08107b1a58fccc75a9bcfbe88342f0d1a512c17
Meenoow <100647073+Meenoow@users.noreply.github.com>
2022-04-24 16:04:52 -0500

    Forecast3
    Created new functions to loop the HTML code. this allowed to imbed and call the ip and returns the live info for the week in any city. Index added to only show 6 days instead of 8.

--- src/app.js
+++ src/app.js
@@ -74,29 +74,41 @@ function formatDate(date) {
 
 let celsiusTemperature = null
 //forecast
+function formatDay(timestamp) {
+ let date = new Date(timestamp * 1000);
+ let day = date.getDay();
+ let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+ return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-days.forEach(function(day) {
+forecast.forEach(function(forecastDay, index) {
+  if (index <6) {
   forecastHTML = 
 forecastHTML + 
 `
 <div class="col-2"> 
-  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-date">${formatDay
+    (forecastDay.dt)}</div>
+    ${index}
     <img 
-    src="http://openweathermap.org/img/wn/01d@2x.png"
+    src="http://openweathermap.org/img/wn/${forecastDay.weather
+    [0].icon}@2x.png"
     alt=""
     width="36"/>
     <br>
     <div class="weather-forecast-temp">
-      <span clas="weather-forecast-temp-max">18˚</span> 
-      <span class="weather-forecast-temp-min">12˚ </span>
+      <span clas="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}˚</span> 
+      <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}˚ </span>
   </div>
 </div>
-`;
+`;}
 });
 
 forecastHTML = forecastHTML + `</div>`;
