911d600f01a70b725eb384e657e52a15555a2cc3
Meenoow <100647073+Meenoow@users.noreply.github.com>
2022-04-24 16:31:08 -0500

    Forecast4
    Forecast fully functional. Removed index shown.

--- src/app.js
+++ src/app.js
@@ -96,7 +96,6 @@ forecastHTML +
 <div class="col-2"> 
   <div class="weather-forecast-date">${formatDay
     (forecastDay.dt)}</div>
-    ${index}
     <img 
     src="http://openweathermap.org/img/wn/${forecastDay.weather
     [0].icon}@2x.png"
