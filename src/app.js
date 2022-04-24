0ab02f5812c672eae44e63655e2c042e05bf965f
Meenoow <100647073+Meenoow@users.noreply.github.com>
2022-04-24 15:38:07 -0500

    Forecast2
    In display forecast, specified the coordinates and added api key to collect live data

--- src/app.js
+++ src/app.js
@@ -74,11 +74,12 @@ function formatDate(date) {
 
 let celsiusTemperature = null
 //forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
-let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
 days.forEach(function(day) {
   forecastHTML = 
 forecastHTML + 
@@ -102,12 +103,11 @@ forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
 
+
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "0d55405ea37f9b16a55f03b2fb1326a2";
-  let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
   let dateElement = document.querySelector("#date");
